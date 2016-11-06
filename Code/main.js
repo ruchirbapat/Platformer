f1f196c9faa99f82141e4c59bb0b73fbca1b52bb
Ruchir Bapat <ruchir.bapat@gmail.com>
2016-11-07 10:23:48 +1100

    Added Animation Frame Requester

--- Code/main.js
+++ Code/main.js
@@ -1,1 +1,15 @@
-var canvas = document.getElementById("canvas");
+(function() 
+{
+    if (!window.requestAnimationFrame) 
+    { 
+        window.requestAnimationFrame = window.webkitRequestAnimationFrame || 
+        window.mozRequestAnimationFrame    || 
+        window.oRequestAnimationFrame      || 
+        window.msRequestAnimationFrame     || 
+        
+        function(callback, element) 
+        {
+            window.setTimeout(callback, 1000 / FRAME_RATE);
+        }
+    }
+})();
