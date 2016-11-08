66b2c5e47b561a59d3eb60eb9f2962c8347a3d33
Ruchir Bapat <ruchir.bapat@gmail.com>
2016-11-08 17:03:46 +1100

    Improved collision detection

--- Code/main.js
+++ Code/main.js
@@ -16,12 +16,15 @@ const JUMP_VELOCITY = 15;
 const SPEED = 10;
 const ACCELERATION = 1;
 const FRICTION = 0.9;
-const columns = canvas.height / BOX_SIZE;
-const rows = canvas.width / BOX_SIZE;
+const columns = (canvas.height - BOX_SIZE) / BOX_SIZE;
+const rows = (canvas.width - BOX_SIZE) / BOX_SIZE;
+const fillAmount = 10;
 
 var level = new Array(columns);
 for(var i = 0; i < level.length; i++)
-   level[i] = new Array(rows);
+    level[i] = new Array(rows);
+
+check(level);
 
 var playerSpawnX, playerSpawnY;
 playerSpawnX = canvas.width / 2;
@@ -36,34 +39,34 @@ check(player);
 box = new Box(0, canvas.height - BOX_SIZE, canvas.width, BOX_SIZE, "rgb(100, 100, 255)");
 
 for(var x = 0; x < columns; x++) {
-   for(var y = 0; y < rows; y++) {
-      var randVal = randomNumber(1, 100);
-      level[x][y] = ((randVal >= 90) ? 1 : 0);
-   }
+    for(var y = 0; y < rows; y++) {
+        var randVal = randomNumber(1, 101);
+        level[x][y] = ((randVal <= fillAmount) ? 1 : 0);
+    }
 }
 
 for(var x = 1; x < columns; x++) {
-   for(var y = 1  ; y < rows; y++) {
-      console.log("The value at level[" + x + "][" + y + "] is " + level[x][y]);
-   }
+    for(var y = 1  ; y < rows; y++) {
+        console.log("The value at level[" + x + "][" + y + "] is " + level[x][y]);
+    }
 }
 
 for(var x = 1; x < columns; x++) {
-   for(var y = 1; y < rows; y++) {
-      if(level[x][y] === 1)
-         box = new Box(x * columns, y * rows, BOX_SIZE, BOX_SIZE, "rgba(100, 100, 255, 0.5)")
-   }
+    for(var y = 1; y < rows; y++) {
+        if(level[x][y] === 1)
+            box = new Box(x * columns, y * rows, BOX_SIZE, BOX_SIZE, "rgba(100, 100, 255, 0.5)")
+        }
 }
 
 
 (function() {
     if (!window.requestAnimationFrame) {
         window.requestAnimationFrame = window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
 
-        function(callback, element) {
+            function(callback, element) {
             window.setTimeout(callback, 1000 / FRAME_RATE);
         }
     }
@@ -71,139 +74,139 @@ for(var x = 1; x < columns; x++) {
 console.log("Animation frame requested.");
 
 function Player(xPos, yPos, _width, _height, _renderColour) {
-   this.x = xPos;
-   this.y = yPos;
-   this.width = _width;
-   this.height = _height;
-   this.colour = _renderColour;
-   this.xVelocity = 0;
-   this.yVelocity = 0;
-   this.grounded = false;
-   this.jumping = false;
-   this.doubleJumping = false;
-   this.jumpCount = 0;
+    this.x = xPos;
+    this.y = yPos;
+    this.width = _width;
+    this.height = _height;
+    this.colour = _renderColour;
+    this.xVelocity = 0;
+    this.yVelocity = 0;
+    this.grounded = false;
+    this.jumping = false;
+    this.doubleJumping = false;
+    this.jumpCount = 0;
 
-   this.toString = function() {
-      console.log("Player's position: (" + this.x + ", " + this.y + ")");
-      console.log("Player's dimensions: (" + this.width + ", " + this.height + ")");
-      console.log("Player's colour: " + this.colour);
-      console.log("Player's velocity: (" + this.xVelocity + ", " + this.yVelocity + ")");
-   }
+    this.toString = function() {
+        console.log("Player's position: (" + this.x + ", " + this.y + ")");
+        console.log("Player's dimensions: (" + this.width + ", " + this.height + ")");
+        console.log("Player's colour: " + this.colour);
+        console.log("Player's velocity: (" + this.xVelocity + ", " + this.yVelocity + ")");
+    }
 
-   this.respawn = function() {
-      this.x = playerSpawnX;
-      this.y = playerSpawnY;
-   }
+    this.respawn = function() {
+        this.x = playerSpawnX;
+        this.y = playerSpawnY;
+    }
 
-   this.respawn = function(x, y) {
-      this.x = x;
-      this.y = y;
-   }
+    this.respawn = function(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
-   this.setVelocity = function(x, y) {
-      this.xVelocity = x;
-      this.yVelocity = y;
-   }
+    this.setVelocity = function(x, y) {
+        this.xVelocity = x;
+        this.yVelocity = y;
+    }
 
-   this.calculateVelocity = function() {
-      if(input.getKeyDown(keyCode.upArrow) && this.grounded)
-         this.setVelocity(0, -SPEED);
+    this.calculateVelocity = function() {
+        if(input.getKeyDown(keyCode.upArrow) && this.grounded)
+            this.setVelocity(0, -SPEED);
 
-      if(input.getKeyDown(keyCode.rightArrow))
-         this.setVelocity(SPEED, 0);
+        if(input.getKeyDown(keyCode.rightArrow))
+            this.setVelocity(SPEED, 0);
 
-      if(input.getKeyDown(keyCode.leftArrow))
-         this.setVelocity(-SPEED, 0);
-   }
+        if(input.getKeyDown(keyCode.leftArrow))
+            this.setVelocity(-SPEED, 0);
+    }
 
-   this.adjustVelocityByCollision = function(toTest) {
-      if((collisionChecker.testCollision(this, toTest) === "b") || this.y === canvas.height - BOX_SIZE) {
-         this.setVelocity(this.xVelocity, 0);
-         this.grounded = true;
-         this.jumping = false;
-      }
-   }
+    this.adjustVelocityByCollision = function(toTest) {
+        if((collisionChecker.testCollision(this, toTest) === "b") || this.y === canvas.height - BOX_SIZE) {
+            this.setVelocity(this.xVelocity, 0);
+            this.grounded = true;
+            this.jumping = false;
+        }
+    }
 
-   this.applyVelocity = function() {
-      this.x += this.xVelocity;
-      this.y += this.yVelocity;
-      this.y += GRAVITY;
-   }
+    this.applyVelocity = function() {
+        this.x += this.xVelocity;
+        this.y += this.yVelocity;
+        this.y += GRAVITY;
+    }
 
-   this.resetVelocity = function() {
-      this.xVelocity = 0;
-      this.yVelocity = 0;
-   }
+    this.resetVelocity = function() {
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+    }
 
-   this.constrain = function() {
-      this.x = clamp(this.x, 0, canvas.width - BOX_SIZE);
-      this.y = clamp(this.y, 0, canvas.height - BOX_SIZE);
-   }
+    this.constrain = function() {
+        this.x = clamp(this.x, 0, canvas.width - BOX_SIZE);
+        this.y = clamp(this.y, 0, canvas.height - BOX_SIZE);
+    }
 
-   this.update = function() {
-      this.resetVelocity();
-      this.calculateVelocity();
-      this.applyVelocity();
-      this.constrain();
-      this.resetVelocity();
-   }
+    this.update = function() {
+        this.resetVelocity();
+        this.calculateVelocity();
+        this.applyVelocity();
+        this.constrain();
+        this.resetVelocity();
+    }
 
-   this.move = function() {
-      if(input.getKeyDown(keyCode.upArrow) || input.getKeyDown(keyCode.spacebarKey)) {
-         if((!player.jumping) && player.grounded) {
-            player.yVelocity = -JUMP_VELOCITY;
-            player.grounded = false;
-            player.jumping = true;
-            player.doubleJumping = false;
-            player.jumpCount++;
-         }
+    this.move = function() {
+        if(input.getKeyDown(keyCode.upArrow) || input.getKeyDown(keyCode.spacebarKey)) {
+            if((!player.jumping) && player.grounded) {
+                player.yVelocity = -JUMP_VELOCITY;
+                player.grounded = false;
+                player.jumping = true;
+                player.doubleJumping = false;
+                player.jumpCount++;
+            }
 
-      }
+        }
 
-      if(input.getKeyDown(keyCode.leftArrow) && player.xVelocity > -SPEED)
-         player.xVelocity -= ACCELERATION;
+        if(input.getKeyDown(keyCode.leftArrow) && player.xVelocity > -SPEED)
+            player.xVelocity -= ACCELERATION;
 
-      if(input.getKeyDown(keyCode.rightArrow) && player.xVelocity < SPEED)
-         player.xVelocity += ACCELERATION;
+        if(input.getKeyDown(keyCode.rightArrow) && player.xVelocity < SPEED)
+            player.xVelocity += ACCELERATION;
 
-      player.xVelocity *= FRICTION;
-      player.yVelocity += GRAVITY;
+        player.xVelocity *= FRICTION;
+        player.yVelocity += GRAVITY;
 
-      player.y += player.yVelocity;
-      player.x += player.xVelocity;
-   }
+        player.y += player.yVelocity;
+        player.x += player.xVelocity;
+    }
 
-   this.render = function() {
-      context.fillStyle = this.colour;
-      context.fillRect(this.x, this.y, this.width, this.height);
-   }
+    this.render = function() {
+        context.fillStyle = this.colour;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
 
 function Box(xPos, yPos, _width, _height, _renderColour) {
-   this.x = xPos;
-   this.y = yPos;
-   this.width = _width;
-   this.height = _height;
-   this.colour = _renderColour;
+    this.x = xPos;
+    this.y = yPos;
+    this.width = _width;
+    this.height = _height;
+    this.colour = _renderColour;
 
-   boxes.push(this);
+    boxes.push(this);
 
-   this.toString = function() {
-      console.log("Box's position: (" + this.x + ", " + this.y + ")");
-      console.log("Box's dimensions: (" + this.width + ", " + this.height + ")");
-      console.log("Box's colour: " + this.colour);
-   }
+    this.toString = function() {
+        console.log("Box's position: (" + this.x + ", " + this.y + ")");
+        console.log("Box's dimensions: (" + this.width + ", " + this.height + ")");
+        console.log("Box's colour: " + this.colour);
+    }
 
-   this.constrain = function() {
-      this.x = clamp(this.x, 0, canvas.width - BOX_SIZE);
-      this.y = clamp(this.y, 0, canvas.height - BOX_SIZE);
-   }
+    this.constrain = function() {
+        this.x = clamp(this.x, 0, canvas.width - BOX_SIZE);
+        this.y = clamp(this.y, 0, canvas.height - BOX_SIZE);
+    }
 
-   this.render = function() {
-      context.fillStyle = this.colour;
-      context.fillRect(this.x, this.y, this.width, this.height);
-   }
+    this.render = function() {
+        context.fillStyle = this.colour;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
 function onKeyDown(key) {
@@ -216,173 +219,180 @@ window.addEventListener("keydown", onKeyDown, false);
 console.log("keydown Event Listener set.");
 
 function onKeyUp(key) {
-   delete heldKeys[key.keyCode];
+    delete heldKeys[key.keyCode];
 }
 window.addEventListener("keyup", onKeyUp, false);
 console.log("keyup Event Listener set.");
 
 console.log("About to render first frame!");
 function update() {
-   for(var i = 0; i < boxes.length; i++)
-      boxes[i].render();
+    for(var i = 0; i < boxes.length; i++)
+        boxes[i].render();
 
-   player.move();
+    
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-   context.clearRect(0, 0, canvas.width, canvas.height);
+    for(var i = 0; i < boxes.length; i++) {
+        player.grounded = true;
 
-   for(var i = 0; i < boxes.length; i++) {
+        if(collisionChecker.quickBoxTest(player, boxes[i])) {
+            
+            var collDir = collisionChecker.testCollision(player, boxes[i]);
 
-      player.grounded = true;
+            if(collDir === "r" || collDir === "l" || player.y === (canvas.height - BOX_SIZE)) {
+                player.xVelocity = 0;
+                player.jumping = false;
+                player.doubleJumping = false;
+                player.jumpCount = 0;
+                //console.log("Player received a collision on the " + ((collDir === "r") ? "right" : "left") + "!");
+            }
+            if(collDir === "b") {
+                player.grounded = true;
+                player.yVelocity = 0;
+                player.jumping = false;
+                player.doubleJumping = false;
+                player.jumpCount = 0;
+                //console.log("Player received a collision on the bottom!");
+            }
+            if(collDir === "t") {
+                player.yVelocity = 0;
+            }
+        }
+        render(boxes[i]);
+    }
+    player.move();
 
-      var collDir = collisionChecker.testCollision(player, boxes[i]);
-
-      if(collDir === "r" || collDir === "l" || player.y === (canvas.height - BOX_SIZE)) {
-         player.xVelocity = 0;
-         player.jumping = false;
-         player.doubleJumping = false;
-         player.jumpCount = 0;
-         console.log("Player received a collision on the " + ((collDir === "r") ? "right" : "left") + "!");
-      }
-      if(collDir === "b") {
-         player.grounded = true;
-         player.yVelocity = 0;
-         player.jumping = false;
-         player.doubleJumping = false;
-         player.jumpCount = 0;
-         console.log("Player received a collision on the bottom!");
-      }
-      if(collDir === "t") {
-         player.yVelocity = 0;
-      }
-      render(boxes[i]);
-   }
-   render(player);
+    render(player);
 }
 
 function game() {
-   update();
-   requestAnimationFrame(game);
+    update();
+    requestAnimationFrame(game);
 }
 window.addEventListener("load", game);
 
 
 function clamp(value, min, max) {
-   if(value > max)
-      return max;
-   else if(value < min)
-      return min
+    if(value > max)
+        return max;
+    else if(value < min)
+        return min
 
-   return value;
+        return value;
 }
 
 function render(object) {
-   object.x = clamp(object.x, 0, canvas.width - BOX_SIZE);
-   object.y = clamp(object.y, 0, canvas.height - BOX_SIZE);
-   context.fillStyle = object.colour;
-   context.fillRect(object.x, object.y, object.width, object.height, object.colour);
+    object.x = clamp(object.x, 0, canvas.width - BOX_SIZE);
+    object.y = clamp(object.y, 0, canvas.height - BOX_SIZE);
+    context.fillStyle = object.colour;
+    context.fillRect(object.x, object.y, object.width, object.height, object.colour);
 }
 
 function check(object) {
-   if(!object || object === null) {
-      console.error(object.constructor.name + " is NULL.");
-      return false;
-   } else {
-      //console.group("Properties of " + object.constructor.name + ": ");
-      console.log(object.constructor.name + " has been created.")
-      return true;
-   }
+    if(!object || object === null) {
+        console.error(object.constructor.name + " is NULL.");
+        return false;
+    } else {
+        //console.group("Properties of " + object.constructor.name + ": ");
+        console.log(object.constructor.name + " has been created.")
+        return true;
+    }
 }
 
 var collisionChecker = {
-   quickBoxTest: function(boxA, boxB) {
-      if(boxA.x < boxB.x + boxB.width && boxA.x + boxA.width > boxB.x && boxA.y < boxB.y + boxB.height && boxA.height + boxA.y > boxB.y)
-         return true;
+    quickBoxTest: function(boxA, boxB) {
+        if(boxA.x < boxB.x + boxB.width && boxA.x + boxA.width > boxB.x && boxA.y < boxB.y + boxB.height && boxA.height + boxA.y > boxB.y) {
+            //console.log("Quick Box Test between " + boxA.constructor.name + " and " + boxB.constructor.name + " returned true.");
+            return true;
+        }
+        else
+            //console.log("Quick Box Test between " + boxA.constructor.name + " and " + boxB.constructor.name + " returned false.");
 
-      return false;
-   },
+        return false;
+    },
 
-   testCollision: function(objectA, objectB) {
-      //Find the collision vectors
-      var vectorX = (objectA.x + (objectA.width / 2)) - (objectB.x + (objectB.width / 2));
-      var vectorY = (objectA.y + (objectA.height / 2)) - (objectB.y + (objectB.height / 2));
+    testCollision: function(objectA, objectB) {
+        //Find the collision vectors
+        var vectorX = (objectA.x + (objectA.width / 2)) - (objectB.x + (objectB.width / 2));
+        var vectorY = (objectA.y + (objectA.height / 2)) - (objectB.y + (objectB.height / 2));
 
-      //Find the distance between the two objects
-      var deltaWidth = (objectA.width / 2) + (objectB.width / 2);
-      var deltaHeight = (objectA.height / 2) + (objectB.height / 2);
+        //Find the distance between the two objects
+        var deltaWidth = (objectA.width / 2) + (objectB.width / 2);
+        var deltaHeight = (objectA.height / 2) + (objectB.height / 2);
 
-      //Stores the direction of collision
-      var collisionDir = null;
+        //Stores the direction of collision
+        var collisionDir = null;
 
-      //Check if the two objects are intersecting on the x and y axis
-      if(Math.abs(vectorX) < deltaWidth && Math.abs(vectorY) < deltaHeight)
-      {
-         //The direction of collision
-         var directionX = deltaWidth - Math.abs(vectorX);
-         var directionY = deltaHeight - Math.abs(vectorY);
+        //Check if the two objects are intersecting on the x and y axis
+        if(Math.abs(vectorX) < deltaWidth && Math.abs(vectorY) < deltaHeight)
+        {
+            //The direction of collision
+            var directionX = deltaWidth - Math.abs(vectorX);
+            var directionY = deltaHeight - Math.abs(vectorY);
 
-         //Check for vertical collision
-         if(directionX >= directionY)
-         {
-            //Check for collisions from the top
-            if(vectorY > 0)
+            //Check for vertical collision
+            if(directionX >= directionY)
             {
-               objectA.y += directionY;
-               collisionDir = "t";
-            }
+                //Check for collisions from the top
+                if(vectorY > 0)
+                {
+                    objectA.y += directionY;
+                    collisionDir = "t";
+                }
 
-            //Collisions form the botttom
+                //Collisions form the botttom
+                else
+                {
+                    objectA.y -= directionY;
+                    collisionDir = "b";
+                }
+            }
             else
             {
-               objectA.y -= directionY;
-               collisionDir = "b";
-            }
-         }
-         else
-         {
-            //Check for collisions from the left
-            if(vectorX > 0)
-            {
-               objectA.x += directionX;
-               collisionDir = "l";
-            }
+                //Check for collisions from the left
+                if(vectorX > 0)
+                {
+                    objectA.x += directionX;
+                    collisionDir = "l";
+                }
 
-            //Collisions form the right side
-            else
-            {
-               objectA.x -= directionX;
-               collisionDir = "r";
+                //Collisions form the right side
+                else
+                {
+                    objectA.x -= directionX;
+                    collisionDir = "r";
+                }
             }
-         }
-      }
-
-      //Return the direction.
-      return collisionDir;
-   }
+        }
+        
+        //Return the direction.
+        return collisionDir;
+    }
 }
 
 function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 var input = {
-   getKeyDown: function(keyToCheck) {
-      if(heldKeys[keyToCheck] === true)
-         return true
-      else
-         return false;
-   }
+    getKeyDown: function(keyToCheck) {
+        if(heldKeys[keyToCheck] === true)
+            return true
+            else
+                return false;
+    }
 }
 
 var keyCode = {
-   upArrow: 38,
-   downArrow: 40,
-   leftArrow: 37,
-   rightArrow: 39,
+    upArrow: 38,
+    downArrow: 40,
+    leftArrow: 37,
+    rightArrow: 39,
 
-   wKey: 87,
-   aKey: 65,
-   sKey: 83,
-   dKey: 68,
+    wKey: 87,
+    aKey: 65,
+    sKey: 83,
+    dKey: 68,
 
-   spacebarKey: 32
+    spacebarKey: 32
 }
